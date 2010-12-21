7711833a1450479ae5d59bbc3ff8939995eb39a3
Max Bolingbroke <batterseapower@hotmail.com>
2010-12-21 18:57:19 +0000

    Improved documentation, version bounds, added function. All at once. Oops

--- System/IO/Temp.hs
+++ System/IO/Temp.hs
@@ -18,11 +18,38 @@ import System.IO
 import Distribution.Compat.TempFile
 
 
+-- | Create and use a temporary directory in the system standard temporary directory.
+--
+-- Behaves exactly the same as 'withTempDirectory', except that the parent temporary directory
+-- will be that returned by 'getTemporaryDirectory'.
+withSystemTempFile :: String   -- ^ File name template. See 'openTempFile'.
+                   -> (FilePath -> Handle -> IO a) -- ^ Callback that can use the file
+                   -> IO a
+withSystemTempFile template action = getTemporaryDirectory >>= \tmpDir -> withTempFile tmpDir template action
+
+-- | Create and use a temporary directory in the system standard temporary directory.
+--
+-- Behaves exactly the same as 'withTempDirectory', except that the parent temporary directory
+-- will be that returned by 'getTemporaryDirectory'.
+withSystemTempDirectory :: String   -- ^ Directory name template. See 'openTempFile'.
+                        -> (FilePath -> IO a) -- ^ Callback that can use the directory
+                        -> IO a
+withSystemTempDirectory template action = getTemporaryDirectory >>= \tmpDir -> withTempDirectory tmpDir template action
+
+
 -- | Use a temporary filename that doesn't already exist.
 --
+-- Creates a new temporary file inside the given directory, making use of the
+-- template. The temp file is deleted after use. For example:
+--
+-- > withTempFile "src" "sdist." $ \tmpFile hFile -> do ...
+--
+-- The @tmpFlie@ will be file in the given directory, e.g.
+-- @src/sdist.342@.
 withTempFile :: FilePath -- ^ Temp dir to create the file in
              -> String   -- ^ File name template. See 'openTempFile'.
-             -> (FilePath -> Handle -> IO a) -> IO a
+             -> (FilePath -> Handle -> IO a) -- ^ Callback that can use the file
+             -> IO a
 withTempFile tmpDir template action =
   Exception.bracket
     (openTempFile tmpDir template)
@@ -38,8 +65,10 @@ withTempFile tmpDir template action =
 --
 -- The @tmpDir@ will be a new subdirectory of the given directory, e.g.
 -- @src/sdist.342@.
---
-withTempDirectory :: FilePath -> String -> (FilePath -> IO a) -> IO a
+withTempDirectory :: FilePath -- ^ Temp directory to create the directory in
+                  -> String   -- ^ Directory name template. See 'openTempFile'.
+                  -> (FilePath -> IO a) -- ^ Callback that can use the directory
+                  -> IO a
 withTempDirectory targetDir template =
   Exception.bracket
     (createTempDirectory targetDir template)
