2a866b540f71e25b86ccff23359cad22151111b4
Roman Cheplyaka <roma@ro-che.info>
2017-06-11 18:28:04 +0300

    Test that withTempDirectory deletes the directory
    ... even in the presence of async exceptions
    
    Closes #3

--- tests/test.hs
+++ tests/test.hs
@@ -2,6 +2,9 @@
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import Control.Monad
+import Control.Concurrent
+import Control.Concurrent.MVar
 import Control.Exception
 import System.Directory
 import System.IO
@@ -17,6 +20,9 @@ import System.Posix.Files
 import System.IO.Temp
 
 main = do
+  -- force single-thread execution, because changing TMPDIR in one of the
+  -- tests may leak to the other tests
+  setEnv "TASTY_NUM_THREADS" "1"
 #ifndef mingw32_HOST_OS
   setFileCreationMask 0
 #endif
@@ -81,4 +87,25 @@ main = do
         bracket_ (setEnv "TMPDIR" ".") (unsetEnv "TMPDIR") $ do
           withSystemTempFile "temp.txt" $ \fp _ ->
             assertBool "Not absolute" $ isAbsolute fp
+    , testCase "withSystemTempDirectory is not interrupted" $ do
+        -- this mvar is both a channel to pass the name of the directory
+        -- and a signal that we finished creating files and are ready
+        -- to be killed
+        mvar1 <- newEmptyMVar
+        -- this mvar signals that the withSystemTempDirectory function
+        -- returned and we can check whether the directory has survived
+        mvar2 <- newEmptyMVar
+        threadId <- forkIO $
+          (withSystemTempDirectory "temp.test." $ \dir -> do
+            replicateM_ 100 $ emptyTempFile dir "file.xyz"
+            putMVar mvar1 dir
+            threadDelay $ 10^6
+          ) `finally` (putMVar mvar2 ())
+        dir <- readMVar mvar1
+        -- start sending exceptions
+        replicateM_ 10 $ forkIO $ killThread threadId
+        -- wait for the thread to finish
+        readMVar mvar2
+        -- check whether the directory was successfully removed
+        assertBool "Directory was not removed" . not =<< doesDirectoryExist dir
     ]
