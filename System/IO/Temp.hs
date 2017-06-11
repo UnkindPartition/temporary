a11e8315816889582445d9d8c4b7d7035e9480a5
Roman Cheplyaka <roma@ro-che.info>
2017-06-11 11:38:43 +0300

    Modernize the code
    - Remove ifdefs for unsupported compilers
    - Use new functions when available
    - Remove Distribution.Compat modules
    - Update the docs

--- System/IO/Temp.hs
+++ System/IO/Temp.hs
@@ -1,30 +1,34 @@
+{-# LANGUAGE CPP #-}
 module System.IO.Temp (
     withSystemTempFile, withSystemTempDirectory,
     withTempFile, withTempDirectory,
-    module Distribution.Compat.TempFile
+    openNewBinaryFile,
+    createTempDirectory,
+    -- * Re-exports from System.IO
+    openTempFile,
+    openBinaryTempFile
   ) where
 
--- NB: this module was extracted directly from "Distribution/Simple/Utils.hs"
--- in a Cabal tree whose most recent commit was on Sun Oct 10 22:00:26
---
--- The files in the Distribution/Compat tree are exact copies of the corresponding
--- file in the Cabal checkout.
-
-
-import Control.Monad.Catch as Exception
+import qualified Control.Monad.Catch as MC
 
 import Control.Monad.IO.Class
 import System.Directory
-import System.IO
-
-import Distribution.Compat.TempFile
-
+import System.IO (Handle, hClose, openTempFile, openBinaryTempFile,
+       openBinaryTempFileWithDefaultPermissions)
+import System.IO.Error        (isAlreadyExistsError)
+import System.Posix.Internals (c_getpid)
+import System.FilePath        ((</>))
+#ifdef mingw32_HOST_OS
+import System.Directory       ( createDirectory )
+#else
+import qualified System.Posix
+#endif
 
 -- | Create and use a temporary file in the system standard temporary directory.
 --
 -- Behaves exactly the same as 'withTempFile', except that the parent temporary directory
 -- will be that returned by 'getTemporaryDirectory'.
-withSystemTempFile :: (MonadIO m, MonadMask m) =>
+withSystemTempFile :: (MonadIO m, MC.MonadMask m) =>
                       String   -- ^ File name template. See 'openTempFile'.
                    -> (FilePath -> Handle -> m a) -- ^ Callback that can use the file
                    -> m a
@@ -34,7 +38,7 @@ withSystemTempFile template action = liftIO getTemporaryDirectory >>= \tmpDir ->
 --
 -- Behaves exactly the same as 'withTempDirectory', except that the parent temporary directory
 -- will be that returned by 'getTemporaryDirectory'.
-withSystemTempDirectory :: (MonadIO m, MonadMask m) =>
+withSystemTempDirectory :: (MonadIO m, MC.MonadMask m) =>
                            String   -- ^ Directory name template. See 'openTempFile'.
                         -> (FilePath -> m a) -- ^ Callback that can use the directory
                         -> m a
@@ -50,13 +54,13 @@ withSystemTempDirectory template action = liftIO getTemporaryDirectory >>= \tmpD
 --
 -- The @tmpFile@ will be file in the given directory, e.g.
 -- @src/sdist.342@.
-withTempFile :: (MonadIO m, MonadMask m) =>
+withTempFile :: (MonadIO m, MC.MonadMask m) =>
                 FilePath -- ^ Temp dir to create the file in
              -> String   -- ^ File name template. See 'openTempFile'.
              -> (FilePath -> Handle -> m a) -- ^ Callback that can use the file
              -> m a
 withTempFile tmpDir template action =
-  Exception.bracket
+  MC.bracket
     (liftIO (openTempFile tmpDir template))
     (\(name, handle) -> liftIO (hClose handle >> ignoringIOErrors (removeFile name)))
     (uncurry action)
@@ -70,15 +74,46 @@ withTempFile tmpDir template action =
 --
 -- The @tmpDir@ will be a new subdirectory of the given directory, e.g.
 -- @src/sdist.342@.
-withTempDirectory :: (MonadMask m, MonadIO m) =>
+withTempDirectory :: (MC.MonadMask m, MonadIO m) =>
                      FilePath -- ^ Temp directory to create the directory in
                   -> String   -- ^ Directory name template. See 'openTempFile'.
                   -> (FilePath -> m a) -- ^ Callback that can use the directory
                   -> m a
 withTempDirectory targetDir template =
-  Exception.bracket
+  MC.bracket
     (liftIO (createTempDirectory targetDir template))
     (liftIO . ignoringIOErrors . removeDirectoryRecursive)
 
-ignoringIOErrors :: MonadCatch m => m () -> m ()
-ignoringIOErrors ioe = ioe `Exception.catch` (\e -> const (return ()) (e :: IOError))
+ignoringIOErrors :: MC.MonadCatch m => m () -> m ()
+ignoringIOErrors ioe = ioe `MC.catch` (\e -> const (return ()) (e :: IOError))
+
+-- | Like 'openBinaryTempFile', but uses 666 rather than 600 for the
+-- permissions.
+--
+-- Equivalent to 'openBinaryTempFileWithDefaultPermissions'.
+openNewBinaryFile :: FilePath -> String -> IO (FilePath, Handle)
+openNewBinaryFile = openBinaryTempFileWithDefaultPermissions
+
+-- | Create a temporary directory. See 'withTempDirectory'.
+createTempDirectory
+  :: FilePath -- ^ Temp directory to create the directory in
+  -> String -- ^ Directory name template
+  -> IO FilePath
+createTempDirectory dir template = do
+  pid <- c_getpid
+  findTempName pid
+  where
+    findTempName x = do
+      let dirpath = dir </> template ++ show x
+      r <- MC.try $ mkPrivateDir dirpath
+      case r of
+        Right _ -> return dirpath
+        Left  e | isAlreadyExistsError e -> findTempName (x+1)
+                | otherwise              -> ioError e
+
+mkPrivateDir :: String -> IO ()
+#ifdef mingw32_HOST_OS
+mkPrivateDir s = createDirectory s
+#else
+mkPrivateDir s = System.Posix.createDirectory s 0o700
+#endif
